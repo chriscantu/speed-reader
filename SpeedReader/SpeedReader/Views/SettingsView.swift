4aa35f88170ff16da81221dcac9bae4edac65dc1
Chris Cantu <chris.cantu@procore.com>
2026-04-04 13:08:25 -0500

    Fix New York font name in SwiftUI preview (space required)

--- SpeedReader/SpeedReader/Views/SettingsView.swift
+++ SpeedReader/SpeedReader/Views/SettingsView.swift
@@ -6,7 +6,7 @@ extension ReaderFont {
         switch self {
         case .system: return .system(size: size, weight: .regular)
         case .openDyslexic: return .custom("OpenDyslexic", size: size)
-        case .newYork: return .custom("NewYork", size: size)
+        case .newYork: return .custom("New York", size: size)
         case .georgia: return .custom("Georgia", size: size)
         case .menlo: return .custom("Menlo", size: size)
         }
